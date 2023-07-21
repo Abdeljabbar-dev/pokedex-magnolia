ca638b3531411398260dc1190989521448e41593
Abdeljabbar-dev <jaddiabdeljabbar@gmail.com>
2023-07-21 17:03:12 +0200

    reconfigure

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import PokemonList from "@/components/PokemonList.vue";
 
-describe("HelloWorld.vue", () => {
+describe("PokemonList.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(PokemonList, {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
