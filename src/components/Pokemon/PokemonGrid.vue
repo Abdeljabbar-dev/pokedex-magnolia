f38e14fd9370c793bfa7864d6695b0729c314b4b
Abdeljabbar-dev <jaddiabdeljabbar@gmail.com>
2023-07-21 18:20:28 +0200

    refresh git

---
+++ src/components/Pokemon/PokemonGrid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid xl:grid-cols-4 gap-8">
+    <div v-for="(result, index) in results" :key="index">
+      <PokemonCard :name="result.name" :url="result.url" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import PokemonCard from "./PokemonCard.vue";
+defineProps({
+  results: Array,
+});
+</script>
